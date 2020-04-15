d09ea57c806fdc3951f4aa26500d96cac2477a33
francoisd-code <58399917+francoisd-code@users.noreply.github.com>
2020-04-15 11:53:36 +0100

    Add files via upload
    Added new GUI classes Log and PlayerDetails

---
+++ PlayerDetails.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javafx.scene.layout.VBox;
+
+/**
+ * The PlayerDetails class represents the current player information. It will
+ * be in a pane on the right side of the GameView.
+ *
+ * @author Francois
+ */
+public class PlayerDetails extends VBox {
+    
+}
